6fb426111f85afaa9bc82b0432ef134794b03eb4
eezhal92 <arkinthesky.69@gmail.com>
2016-10-23 13:05:04 +0700

    trait for run migration

---
+++ src/Testing/DatabaseMigrations.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LaravelDoctrine\Migrations\Testing;
+
+use Illuminate\Contracts\Console\Kernel;
+
+trait DatabaseMigrations
+{
+    /**
+     * Define hooks to migrate the database before and after each test.
+     *
+     * @return void
+     */
+    public function runDatabaseMigrations()
+    {
+        $this->artisan('doctrine:migrations:migrate');
+
+        $this->app[Kernel::class]->setArtisan(null);
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('doctrine:migrations:rollback');
+        });
+    }
+}
