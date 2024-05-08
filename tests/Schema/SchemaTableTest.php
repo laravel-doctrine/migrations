0d2fa2697bd6f4f54bec8f141cdd0e0c3a21fd55
77web <hishida77web@gmail.com>
2024-05-08 23:34:38 +0900

    added missing ->once() call to Mockery stubs

--- tests/Schema/SchemaTableTest.php
+++ tests/Schema/SchemaTableTest.php
@@ -30,35 +30,35 @@ class SchemaTableTest extends TestCase
 
     public function test_guid()
     {
-        $this->dbal->shouldReceive('addColumn')->with('guid', 'guid');
+        $this->dbal->shouldReceive('addColumn')->with('guid', 'guid')->once();
 
         $this->table->guid('guid');
     }
 
     public function test_primary()
     {
-        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null);
+        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null)->once();
 
         $this->table->primary('id');
     }
 
     public function test_unique()
     {
-        $this->dbal->shouldReceive('addUniqueIndex')->with(['email'], null, []);
+        $this->dbal->shouldReceive('addUniqueIndex')->with(['email'], null, [])->once();
 
         $this->table->unique('email');
     }
 
     public function test_index()
     {
-        $this->dbal->shouldReceive('addIndex')->with(['email'], null, [], []);
+        $this->dbal->shouldReceive('addIndex')->with(['email'], null, [], [])->once();
 
         $this->table->index('email');
     }
 
     public function test_foreign()
     {
-        $this->dbal->shouldReceive('addForeignKeyConstraint')->with('users', ['user_id'], ['id'], [], null);
+        $this->dbal->shouldReceive('addForeignKeyConstraint')->with('users', ['user_id'], ['id'], [], null)->once();
 
         $this->table->foreign('users', 'user_id', 'id');
     }
@@ -72,8 +72,8 @@ class SchemaTableTest extends TestCase
                 'autoIncrement' => true,
                 'unsigned'      => true
             ]
-        );
-        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null);
+        )->once();
+        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null)->once();
 
         $this->table->increments('id');
     }
@@ -87,8 +87,8 @@ class SchemaTableTest extends TestCase
                 'autoIncrement' => true,
                 'unsigned'      => true
             ]
-        );
-        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null);
+        )->once();
+        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null)->once();
 
         $this->table->smallIncrements('id');
     }
@@ -102,21 +102,21 @@ class SchemaTableTest extends TestCase
                 'autoIncrement' => true,
                 'unsigned'      => true
             ]
-        );
-        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null);
+        )->once();
+        $this->dbal->shouldReceive('setPrimaryKey')->with(['id'], null)->once();
 
         $this->table->bigIncrements('id');
     }
 
     public function test_string()
     {
-        $this->dbal->shouldReceive('addColumn')->with('string', 'string', ['length' => 255]);
+        $this->dbal->shouldReceive('addColumn')->with('string', 'string', ['length' => 255])->once();
         $this->table->string('string');
     }
 
     public function test_text()
     {
-        $this->dbal->shouldReceive('addColumn')->with('text', 'text');
+        $this->dbal->shouldReceive('addColumn')->with('text', 'text')->once();
         $this->table->text('text');
     }
 
@@ -125,13 +125,13 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'integer', [
             'autoIncrement' => false,
             'unsigned'      => false
-        ]);
+        ])->once();
         $this->table->integer('column');
 
         $this->dbal->shouldReceive('addColumn')->with('column', 'integer', [
             'autoIncrement' => true,
             'unsigned'      => true
-        ]);
+        ])->once();
         $this->table->integer('column', true, true);
     }
 
@@ -140,13 +140,13 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'smallint', [
             'autoIncrement' => false,
             'unsigned'      => false
-        ]);
+        ])->once();
         $this->table->smallInteger('column');
 
         $this->dbal->shouldReceive('addColumn')->with('column', 'smallint', [
             'autoIncrement' => true,
             'unsigned'      => true
-        ]);
+        ])->once();
         $this->table->smallInteger('column', true, true);
     }
 
@@ -155,13 +155,13 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'bigint', [
             'autoIncrement' => false,
             'unsigned'      => false
-        ]);
+        ])->once();
         $this->table->bigInteger('column');
 
         $this->dbal->shouldReceive('addColumn')->with('column', 'bigint', [
             'autoIncrement' => true,
             'unsigned'      => true
-        ]);
+        ])->once();
         $this->table->bigInteger('column', true, true);
     }
 
@@ -170,7 +170,7 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'smallint', [
             'autoIncrement' => false,
             'unsigned'      => true
-        ]);
+        ])->once();
         $this->table->unsignedSmallInteger('column');
     }
 
@@ -179,7 +179,7 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'integer', [
             'autoIncrement' => false,
             'unsigned'      => true
-        ]);
+        ])->once();
         $this->table->unsignedInteger('column');
     }
 
@@ -188,7 +188,7 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'bigint', [
             'autoIncrement' => false,
             'unsigned'      => true
-        ]);
+        ])->once();
         $this->table->unsignedBigInteger('column');
     }
 
@@ -197,7 +197,7 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'float', [
             'precision' => 8,
             'scale'     => 2
-        ]);
+        ])->once();
         $this->table->float('column');
     }
 
@@ -206,79 +206,79 @@ class SchemaTableTest extends TestCase
         $this->dbal->shouldReceive('addColumn')->with('column', 'decimal', [
             'precision' => 8,
             'scale'     => 2
-        ]);
+        ])->once();
         $this->table->decimal('column');
     }
 
     public function test_boolean()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'boolean');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'boolean')->once();
         $this->table->boolean('column');
     }
 
     public function test_json()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'json');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'json')->once();
         $this->table->json('column');
     }
 
     public function test_date()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'date');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'date')->once();
         $this->table->date('column');
     }
 
     public function test_dateTime()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'datetime');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'datetime')->once();
         $this->table->dateTime('column');
     }
 
     public function test_dateTimeTz()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'datetimetz');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'datetimetz')->once();
         $this->table->dateTimeTz('column');
     }
 
     public function test_timestamp()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'datetime');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'datetime')->once();
         $this->table->timestamp('column');
     }
 
     public function test_timestampTz()
     {
-        $this->dbal->shouldReceive('addColumn')->with('column', 'datetimetz');
+        $this->dbal->shouldReceive('addColumn')->with('column', 'datetimetz')->once();
         $this->table->timestampTz('column');
     }
 
     public function test_nullable_timestamps()
     {
         $column = m::mock(Column::class);
-        $this->dbal->shouldReceive('addColumn')->with('created_at', 'datetime')->andReturn($column);
-        $this->dbal->shouldReceive('addColumn')->with('updated_at', 'datetime')->andReturn($column);
+        $this->dbal->shouldReceive('addColumn')->with('created_at', 'datetime')->andReturn($column)->once();
+        $this->dbal->shouldReceive('addColumn')->with('updated_at', 'datetime')->andReturn($column)->once();
         $column->shouldReceive('setNotnull')->with(false)->twice();
         $this->table->nullableTimestamps();
     }
 
     public function test_timestamps()
     {
-        $this->dbal->shouldReceive('addColumn')->with('created_at', 'datetime');
-        $this->dbal->shouldReceive('addColumn')->with('updated_at', 'datetime');
+        $this->dbal->shouldReceive('addColumn')->with('created_at', 'datetime')->once();
+        $this->dbal->shouldReceive('addColumn')->with('updated_at', 'datetime')->once();
         $this->table->timestamps();
     }
 
     public function test_timestampsTz()
     {
-        $this->dbal->shouldReceive('addColumn')->with('created_at', 'datetimetz');
-        $this->dbal->shouldReceive('addColumn')->with('updated_at', 'datetimetz');
+        $this->dbal->shouldReceive('addColumn')->with('created_at', 'datetimetz')->once();
+        $this->dbal->shouldReceive('addColumn')->with('updated_at', 'datetimetz')->once();
         $this->table->timestampsTz();
     }
 
     public function test_softdeletes()
     {
         $column = m::mock(Column::class);
-        $this->dbal->shouldReceive('addColumn')->with('deleted_at', 'datetime')->andReturn($column);
+        $this->dbal->shouldReceive('addColumn')->with('deleted_at', 'datetime')->andReturn($column)->once();
         $column->shouldReceive('setNotnull')->with(false)->once();
         $this->table->softDeletes();
     }
@@ -286,7 +286,7 @@ class SchemaTableTest extends TestCase
     public function test_binary()
     {
         $column = m::mock(Column::class);
-        $this->dbal->shouldReceive('addColumn')->with('column', 'binary', ['length' => 255])->andReturn($column);
+        $this->dbal->shouldReceive('addColumn')->with('column', 'binary', ['length' => 255])->andReturn($column)->once();
         $column->shouldReceive('setNotnull')->with(false)->once();
         $this->table->binary('column');
     }
@@ -295,7 +295,7 @@ class SchemaTableTest extends TestCase
     {
         $column = m::mock(Column::class);
         $this->dbal->shouldReceive('addColumn')->with('remember_token', 'string', ['length' => 100])
-                   ->andReturn($column);
+                   ->andReturn($column)->once();
         $column->shouldReceive('setNotnull')->with(false)->once();
         $this->table->rememberToken('column');
     }
@@ -307,7 +307,7 @@ class SchemaTableTest extends TestCase
 
     public function test_drop_column()
     {
-        $this->dbal->shouldReceive('dropColumn')->with('column');
+        $this->dbal->shouldReceive('dropColumn')->with('column')->once();
 
         $this->table->dropColumn('column');
     }
